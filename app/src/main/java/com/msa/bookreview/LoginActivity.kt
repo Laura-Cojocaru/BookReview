e3cf8a49a24351e49c8bb157214a8f0d18f27d5a
LauraCojocaru <lauracojocaru1997@gmail.com>
2019-12-05 01:35:22 +0200

    Picture taking
    Added Picture taking

--- app/src/main/java/com/msa/bookreview/LoginActivity.kt
+++ app/src/main/java/com/msa/bookreview/LoginActivity.kt
@@ -27,27 +27,9 @@ class LoginActivity : AppCompatActivity() {
     lateinit var providers : List<AuthUI.IdpConfig>
     val MY_REQUEST_CODE: Int = 7117
 
-    private lateinit var svScanner: SurfaceView
-    private lateinit var tvText: TextView
-
-    private lateinit var cameraSource: CameraSource
-    private lateinit var textRecognizer: TextRecognizer
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
-        svScanner = findViewById(R.id.sv_scanner)
-        tvText = findViewById(R.id.tv_text)
-
-        textRecognizer = TextRecognizer.Builder(this).build()
-        if (textRecognizer.isOperational){
-            //start scanning
-            startScanner()
-        } else {
-            Log.w("Hi", "Your text recognizer is not operational")
-        }
 
         //Init
         providers = Arrays.asList<AuthUI.IdpConfig>(
@@ -59,16 +41,16 @@ class LoginActivity : AppCompatActivity() {
 
 
         //Event
-        btn_sign_out.setOnClickListener {
-            //sign out
-            AuthUI.getInstance().signOut(this@LoginActivity)
-                .addOnCompleteListener{
-                    btn_sign_out.isEnabled = false
-                    showSignInOptions()
-                }
-                .addOnFailureListener{
-                    e-> Toast.makeText(this@LoginActivity, e.message, Toast.LENGTH_SHORT).show()
-                }        }
+//        btn_sign_out.setOnClickListener {
+//            //sign out
+//            AuthUI.getInstance().signOut(this@LoginActivity)
+//                .addOnCompleteListener{
+//                    btn_sign_out.isEnabled = false
+//                    showSignInOptions()
+//                }
+//                .addOnFailureListener{
+//                    e-> Toast.makeText(this@LoginActivity, e.message, Toast.LENGTH_SHORT).show()
+//                }        }
 
 //        btn_scan.setOnClickListener {
 //            val intent = Intent(this, BookActivity::class.java)
@@ -85,7 +67,7 @@ class LoginActivity : AppCompatActivity() {
                 val user = FirebaseAuth.getInstance().currentUser
                 Toast.makeText(this, ""+user!!.email,Toast.LENGTH_SHORT).show()
 
-                btn_sign_out.isEnabled = true
+//                btn_sign_out.isEnabled = true
             }else{
                 Toast.makeText(this, ""+response!!.error!!.message,Toast.LENGTH_SHORT).show()
             }
@@ -100,62 +82,4 @@ class LoginActivity : AppCompatActivity() {
 
     }
 
-    private fun startScanner() {
-        cameraSource = CameraSource.Builder(this, textRecognizer)
-            .setFacing(CameraSource.CAMERA_FACING_BACK).setRequestedFps(30f)
-            .setRequestedPreviewSize(1024, 768).setAutoFocusEnabled(true).build()
-        svScanner.holder.addCallback(object : SurfaceHolder.Callback2{
-            override fun surfaceRedrawNeeded(holder: SurfaceHolder?) {}
-            override fun surfaceChanged(holder: SurfaceHolder?, p1: Int, p2: Int, p3: Int) {}
-            override fun surfaceDestroyed(holder: SurfaceHolder?) {
-                cameraSource.stop()
-            }
-
-            override fun surfaceCreated(holder: SurfaceHolder?) {
-                if(ActivityCompat.checkSelfPermission(this@LoginActivity,
-                        android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
-                    cameraSource.start(holder)
-                } else {
-                    ActivityCompat.requestPermissions(this@LoginActivity, arrayOf(android.Manifest.permission.CAMERA), 123)
-                }
-            }
-        })
-        textRecognizer.setProcessor(object : Detector.Processor<TextBlock> {
-            override fun release() {}
-            override fun receiveDetections(detections: Detector.Detections<TextBlock>?) {
-                val textItems = detections?.detectedItems
-                val builder = StringBuilder()
-                for (i in 0..min(textItems!!.size() - 1, 5)){
-                    var item = textItems.valueAt(i)
-                    builder.append(item.getValue())
-                    builder.append("\n")
-                }
-                tvText.text = builder.toString()
-            }
-
-        })
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode ==123 && grantResults.isNotEmpty()){
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                cameraSource.start(svScanner.holder)
-            } else {
-                Toast.makeText(this, "scanner won't work without permission", Toast.LENGTH_SHORT )
-                    .show()
-            }
-        }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        textRecognizer.release()
-        cameraSource.stop()
-        cameraSource.release()
-    }
 }
--- app/src/main/java/com/msa/bookreview/MainActivity.kt
+++ app/src/main/java/com/msa/bookreview/MainActivity.kt
@@ -3,104 +3,136 @@ package com.msa.bookreview
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-
+import android.Manifest
+import android.app.Activity
+import android.content.ContentValues
+import android.content.Intent
 import android.content.pm.PackageManager
-import android.util.Log
-import android.view.SurfaceHolder
-import android.view.SurfaceView
-import android.widget.TextView
+import android.net.Uri
+import android.os.Build
+//import android.support.v7.app.AppCompatActivity
+import android.provider.MediaStore
 import android.widget.Toast
-import androidx.core.app.ActivityCompat
-import com.google.android.gms.vision.CameraSource
-import com.google.android.gms.vision.Detector
-import com.google.android.gms.vision.text.TextBlock
-import com.google.android.gms.vision.text.TextRecognizer
-
-import kotlin.math.min
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var svScanner: SurfaceView
-    private lateinit var tvText: TextView
+    private val PERMISSION_CODE = 1000;
+    private val IMAGE_CAPTURE_CODE = 1001
+    var image_uri: Uri? = null
 
-    private lateinit var cameraSource: CameraSource
-    private lateinit var textRecognizer: TextRecognizer
+    lateinit var providers : List<AuthUI.IdpConfig>
+    val MY_REQUEST_CODE: Int = 7117
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        setSupportActionBar(toolbar)
 
-        svScanner = findViewById(R.id.sv_scanner)
-        tvText = findViewById(R.id.tv_text)
+        //button click
+        capture_btn.setOnClickListener {
+            //if system os is Marshmallow or Above, we need to request runtime permission
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if (checkSelfPermission(Manifest.permission.CAMERA)
+                    == PackageManager.PERMISSION_DENIED ||
+                    checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_DENIED){
+                    //permission was not enabled
+                    val permission = arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    //show popup to request permission
+                    requestPermissions(permission, PERMISSION_CODE)
+                }
+                else{
+                    //permission already granted
+                    openCamera()
+                }
+            }
+            else{
+                //system os is < marshmallow
+                openCamera()
+            }
+        }
 
-        textRecognizer = TextRecognizer.Builder(this).build()
-        if (textRecognizer.isOperational){
-            //start scanning
-            startScanner()
-        } else {
-            Log.w("Hi", "Your text recognizer is not operational")
+        providers = Arrays.asList<AuthUI.IdpConfig>(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build(),
+            AuthUI.IdpConfig.PhoneBuilder().build()
+        )
+        showSignInOptions()
+
+        //Event
+        btn_sign_out.setOnClickListener {
+            //sign out
+            AuthUI.getInstance().signOut(this@MainActivity)
+                .addOnCompleteListener{
+                    btn_sign_out.isEnabled = false
+                    showSignInOptions()
+                }
+                .addOnFailureListener{
+                        e-> Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
+                }        }
+    }
+
+    private fun showSignInOptions() {
+        startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .setTheme(R.style.MyTheme)
+            .build(),MY_REQUEST_CODE)
+
+    }
+
+    private fun openCamera() {
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, "New Picture")
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera")
+        image_uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+        //camera intent
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri)
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE)
+    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        //called when user presses ALLOW or DENY from Permission Request Popup
+        when(requestCode){
+            PERMISSION_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED){
+                    //permission from popup was granted
+                    openCamera()
+                }
+                else{
+                    //permission from popup was denied
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 
-    private fun startScanner() {
-        cameraSource = CameraSource.Builder(this, textRecognizer)
-            .setFacing(CameraSource.CAMERA_FACING_BACK).setRequestedFps(30f)
-            .setRequestedPreviewSize(1024, 768).setAutoFocusEnabled(true).build()
-        svScanner.holder.addCallback(object : SurfaceHolder.Callback2{
-            override fun surfaceRedrawNeeded(holder: SurfaceHolder?) {}
-            override fun surfaceChanged(holder: SurfaceHolder?, p1: Int, p2: Int, p3: Int) {}
-            override fun surfaceDestroyed(holder: SurfaceHolder?) {
-                cameraSource.stop()
-            }
 
-            override fun surfaceCreated(holder: SurfaceHolder?) {
-                if(ActivityCompat.checkSelfPermission(this@MainActivity,
-                        android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
-                    cameraSource.start(holder)
-                } else {
-                    ActivityCompat.requestPermissions(this@MainActivity, arrayOf(android.Manifest.permission.CAMERA), 123)
-                }
-            }
-        })
-        textRecognizer.setProcessor(object : Detector.Processor<TextBlock> {
-            override fun release() {}
-            override fun receiveDetections(detections: Detector.Detections<TextBlock>?) {
-                val textItems = detections?.detectedItems
-                val builder = StringBuilder()
-                for (i in 0..min(textItems!!.size() - 1, 5)){
-                    var item = textItems.valueAt(i)
-                    builder.append(item.getValue())
-                    builder.append("\n")
-                }
-                tvText.text = builder.toString()
-            }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == MY_REQUEST_CODE){
+            val response = IdpResponse.fromResultIntent(data)
+            if(resultCode == Activity.RESULT_OK){
+                val user = FirebaseAuth.getInstance().currentUser
+                Toast.makeText(this, ""+user!!.email,Toast.LENGTH_SHORT).show()
 
-        })
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode ==123 && grantResults.isNotEmpty()){
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                cameraSource.start(svScanner.holder)
-            } else {
-                Toast.makeText(this, "scanner won't work without permission", Toast.LENGTH_SHORT )
-                    .show()
+                btn_sign_out.isEnabled = true
+            }else{
+                Toast.makeText(this, ""+response!!.error!!.message,Toast.LENGTH_SHORT).show()
             }
         }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        textRecognizer.release()
-        cameraSource.stop()
-        cameraSource.release()
+        if (resultCode == Activity.RESULT_OK){
+            //set image captured to image view
+            image_view.setImageURI(image_uri)
+        }
     }
 
 }
